a1baa57b9d4f2718d45e5fbcb092908b8d9fffb7
happyj <1608877629@qq.com>
2018-03-05 11:13:26 +0800

    第一次提交 3-5

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?48bef8cd7223e0218945e1bc93ebac4a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
